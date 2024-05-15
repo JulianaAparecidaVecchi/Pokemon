6bc178d654315a32d8fbbb6c1ac0f65bd36d0b3c
JulianaAparecidaVecchi <163355132+JulianaAparecidaVecchi@users.noreply.github.com>
2024-05-15 15:19:41 -0300

    Parte 2
    Add funções ,pokébolas e pokémon inicial.
    Além disso foi trocado S/N por 1 e 2 nas escolhas

--- jogo_pokemon.py
+++ jogo_pokemon.py
@@ -39,10 +39,9 @@ def introducao():
     pausa(1)
     print(f"Um mundo de sonhos e aventuras o aguarda! Então, vamos lá!")
     pausa(1)
-    print("Vamos sortear um pokémon inicial para você!")
+    print("Agora escolha um pókemon inicial!")
     linha()
-    print(f"Parabéns! Você recebeu o {sorteio_pokemon_inicial(pokemon_inicial)} em sua pokédex")
-    linha()
+    escolha_pokemon_inicial()
     mostrar_pokedex()
     print(f"Você inicia com {pokebolas} Pokebolas!")
     pausa(1)
@@ -102,12 +101,6 @@ def sorteio_pokemon_aleatorio(pokemon):
 
     return random.choice(pokemon)   
 
-def sorteio_pokemon_inicial(listaPI):
-    PKI=random.choice(listaPI)
-    pokedex.append(PKI)
-    pausa(1)
-    return PKI
-
 def pergunta_genero():
        while True:
             try:
@@ -165,31 +158,37 @@ def pausa(x):
     time.sleep(x)    
 
 def explorar_caverna():
-            global pokebolas,pokemon_aleatorio,pokedex
-            print("Você está adentrando a caverna...")
-            pausa(1)
-            adicionar_pokebola(pokebolas)
-            pokemon_aleatorio=sorteio_pokemon_aleatorio(caverna)
-            print(f"Você entrou na caverna e encontrou um {pokemon_aleatorio}!")
-                          
-            if pokemon_aleatorio in pokedex:
-                print(f"Você já tem o {pokemon_aleatorio} em sua pokédex!")
-            else:                 
-                resposta=input("Deseja tentar capturar esse pokémon (S/N): ")  
-                while True:                 
-                    if resposta=="S" or resposta=="s":
-                        if prob_capturar_pokemon(prob_capt_caverna):
-                            captura_pokemon(pokemon_aleatorio)
-                            break
-                        else:    
-                            print(f"O {pokemon_aleatorio} escapou!")
-                            if pokebolas<=0:
-                                print(f"Suas pokébolas acabaram")
-                                break
-                            else: 
-                                    resposta2=input(f"Quer tentar captura o {pokemon_aleatorio} novamente(S/N): ")
-                                    if resposta2 =="S" or resposta2=="s":
-                                        pokebolas-=1
+    global pokebolas, pokemon_aleatorio, pokedex
+    print("Você está adentrando a caverna...")
+    pausa(1)
+    adicionar_pokebola(pokebolas)
+    pokemon_aleatorio = sorteio_pokemon_aleatorio(caverna)
+    print(f"Você entrou na caverna e encontrou um {pokemon_aleatorio}!")
+    
+    if pokemon_aleatorio in pokedex:
+        print(f"Você já tem o {pokemon_aleatorio} em sua pokédex!")
+        print(f"Voltando para o menu...")
+        pausa(1)
+    else:
+        while True:
+            resposta = input("[1]-Sim\n[2]-Não\nDeseja tentar capturar esse pokémon? ")
+            if resposta.isdigit():
+                resposta = int(resposta)
+                if resposta == 1:
+                    if pokebolas <= 0:
+                        print(f"Suas pokébolas acabaram")
+                        break
+                    if prob_capturar_pokemon(prob_capt_caverna):
+                        captura_pokemon(pokemon_aleatorio)
+                        break
+                    else:
+                        print(f"O {pokemon_aleatorio} escapou!")
+                        if pokebolas > 0:
+                                resposta2 = input(f"[1]-Sim\n[2]-Não\nQuer tentar capturar o {pokemon_aleatorio} novamente? ")
+                                if resposta2.isdigit():
+                                    resposta2=int(resposta2)
+                                    if resposta ==1:    
+                                        pokebolas -= 1
                                         if prob_capturar_pokemon(prob_capt_caverna):
                                             pokedex.append(pokemon_aleatorio)
                                             linha()
@@ -205,20 +204,29 @@ def explorar_caverna():
                                             print("Voltando para o menu...")
                                             pausa(1)
                                             break
-                                    elif resposta2 =="N" or resposta2=="n" :
+                                    elif resposta2==2:
                                         print("Voltando para o menu...")
                                         pausa(1)
                                         break
-                                    else:
-                                        print("Erros, Digite 'S' para sim e 'N' não ")
+                                    elif resposta>2 or resposta<1:
+                                        print("Erro, digite 1 para sim e 2 para não.")
                                         continue
-                                
-                    elif resposta=="N" or resposta=="n":
-                        print("Voltando para o menu...")
-                        pausa(1)
-                        break
-                    else:
-                        print("Erros, Digite 'S' para sim e 'N' não ")
+                                         
+                                else:
+                                    print("Erro, digite 1 para sim e 2 para não.")
+                                    continue
+                            
+                elif resposta == 2:
+                    print("Voltando para o menu...")
+                    pausa(1)
+                    break
+                else:
+                    print("Erro, digite 1 para sim e 2 para não.")
+                    continue
+            else:
+                print("Erro, digite 1 para sim e 2 para não.")
+                continue
+
 
 def explorar_mato():
             global pokebolas,pokemon_aleatorio,pokedex
@@ -230,53 +238,85 @@ def explorar_mato():
                           
             if pokemon_aleatorio in pokedex:
                 print(f"Você já tem o {pokemon_aleatorio} em sua pokédex!")
-            else:                 
-                resposta=input("Deseja tentar capturar esse pokémon (S/N): ")  
-                while True:                 
-                    if resposta=="S" or resposta=="s":
-                        if prob_capturar_pokemon(prob_capt_mato):
-                            captura_pokemon(pokemon_aleatorio)
-                            break
-                        else:    
-                            print(f"O {pokemon_aleatorio} escapou!")
-                            if pokebolas<=0:
-                                print(f"Suas pokébolas acabaram")
-                                break
-                            else: 
-                                    resposta2=input(f"Quer tentar captura o {pokemon_aleatorio} novamente(S/N): ")
-                                    if resposta2 =="S" or resposta2=="s":
-                                        pokebolas-=1
-                                        if prob_capturar_pokemon(prob_capt_mato):
-                                            pokedex.append(pokemon_aleatorio)
-                                            linha()
-                                            print(f"Parabéns!\nVocê capturou o {pokemon_aleatorio}!")
-                                            print(f"Você tem {pokebolas} pokébolas!")
-                                            linha()
-                                            print("Voltando para o menu...")
-                                            pausa(1)
-                                            break
-                                        else:
-                                            print(f"O {pokemon_aleatorio} escapou!")
-                                            print(f"Você tem {pokebolas} pokébolas!")
-                                            print("Voltando para o menu...")
-                                            pausa(1)
-                                            break
-                                    elif resposta2 =="N" or resposta2=="n" :
-                                        print("Voltando para o menu...")
-                                        pausa(1)
+                print(f"Voltando para o menu...")
+                pausa(1)
+            else: 
+                while True:                
+                    resposta = input("[1]-Sim\n[2]-Não\nDeseja tentar capturar esse pokémon? ")                   
+                    if resposta.isdigit():
+                            resposta=int(resposta)
+                            if resposta==1:
+                                if prob_capturar_pokemon(prob_capt_mato):
+                                    captura_pokemon(pokemon_aleatorio)
+                                    break
+                                else:    
+                                    print(f"O {pokemon_aleatorio} escapou!")
+                                    if pokebolas<=0:
+                                        print(f"Suas pokébolas acabaram")
                                         break
-                                    else:
-                                        print("Erros, Digite 'S' para sim e 'N' não ")
-                                        continue
-                                
-                    elif resposta=="N" or resposta=="n":
-                        print("Voltando para o menu...")
-                        pausa(1)
-                        break
-                    else:
-                        print("Erros, Digite 'S' para sim e 'N' não ")         
+                                    else:     
+                                            resposta2 = input(f"[1]-Sim\n[2]-Não\nQuer tentar capturar o {pokemon_aleatorio} novamente? ")
+                                            if resposta2.isdigit():
+                                                resposta2=int(resposta2)
+                                                if resposta2 ==1:
+                                                    pokebolas-=1
+                                                    
+                                                    if prob_capturar_pokemon(prob_capt_mato):
+                                                        pokedex.append(pokemon_aleatorio)
+                                                        linha()
+                                                        print(f"Parabéns!\nVocê capturou o {pokemon_aleatorio}!")
+                                                        print(f"Você tem {pokebolas} pokébolas!")
+                                                        linha()
+                                                        print("Voltando para o menu...")
+                                                        pausa(1)
+                                                        break
+                                                    else:
+                                                        print(f"O {pokemon_aleatorio} escapou!")
+                                                        print(f"Você tem {pokebolas} pokébolas!")
+                                                        print("Voltando para o menu...")
+                                                        pausa(1)
+                                                        break
+                                                elif resposta2==2:
+                                                    print("Voltando para o menu...")
+                                                    pausa(1)
+                                                    break
+                                            elif resposta2==2:
+                                                print("Voltando para o menu...")
+                                                pausa(1)
+                                                break
+                                                     
+                                            elif resposta>2 or resposta<1:
+                                                print("Erro, digite 1 para sim e 2 para não.")
+                                                continue    
+                                    
+                            elif resposta==2:
+                                print("Erro, digite 1 para sim e 2 para não.")
+                                continue 
 
-#Código
+                            elif resposta>2 or resposta<1:
+                                print("Erro, digite 1 para sim e 2 para não.")
+                                continue    
+                    else:
+                        print("Erro, digite 1 para sim e 2 para não.")
+                        continue       
+
+def escolha_pokemon_inicial():
+    print("Escolha o seu Pokémon inicial:")
+    for i, pokemon in enumerate(pokemon_inicial, start=1):
+        print(f"[{i}] {pokemon}")
+
+    escolha_Pok_inc = input("Digite o número correspondente ao Pokémon que você deseja escolher: ")
+    while not escolha_Pok_inc.isdigit() or not (1 <= int(escolha_Pok_inc) <= len(pokemon_inicial)):
+        print("Escolha inválida. Por favor, digite o número correspondente ao Pokémon desejado.")
+        escolha_Pok_inc = input("Digite o número correspondente ao Pokémon que você deseja escolher: ")
+
+    pokemon_escolhido = pokemon_inicial[int(escolha_Pok_inc) - 1]
+    linha()
+    print(f"Você escolheu: {pokemon_escolhido}")
+    print(f"Parabéns! Você recebeu o {pokemon_escolhido} em sua pokédex")
+    linha()
+    pokedex.append(pokemon_escolhido)
+#Programa Principal
 
 introducao()
 menu()
